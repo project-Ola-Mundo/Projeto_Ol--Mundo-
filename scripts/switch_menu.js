ad7b6cb0a4d4d1fdfb400655cbd60321eb17e0fb
juan.souza <juan.souza@urbay.com.br>
2022-03-30 16:32:20 -0300

    add menu e footer mobile

---
+++ scripts/switch_menu.js
@@ -0,0 +1,11 @@
+
+function show_menu(){
+
+    let menu = document.getElementById("dropdown");
+
+    if(menu.style.display != "block"){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none";
+    }
+}
